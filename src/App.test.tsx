760d13b24452f490e59cff4648ace95356619e8d
JasmineZ <xiaoming.zhou@thoughtworks.com>
2022-11-17 13:57:15 +0800

    test: add snapshot test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,10 +2,15 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 import React from 'react';
 import '@testing-library/jest-dom';
+import renderer from 'react-test-renderer';
 
 describe('App test', () => {
   it('should render app', () => {
     render(<App />);
     expect(screen.getByText(/Todo list/i)).toBeInTheDocument();
+  });
+  it('should render the snapshot', () => {
+    const tree = renderer.create(<App />).toJSON();
+    expect(tree).toMatchSnapshot();
   });
 });
