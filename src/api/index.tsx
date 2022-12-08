02b6df7ae22b89b8a8437439a1d4b0c532d75f41
JasmineZ <xiaoming.zhou@thoughtworks.com>
2022-12-08 22:20:13 +0800

    fix: figure the bug that cannot fetch data

--- src/api/index.tsx
+++ src/api/index.tsx
@@ -2,30 +2,30 @@ import { ITask } from '../types';
 import axios from 'axios';
 
 const client = axios.create({
-  baseURL: process.env.REACT_APP_BASEURL,
+  baseURL: 'http://localhost:8080/tasks',
+  // headers: {
+  //   Accept: 'application/json',
+  // },
 });
 
 export const getTasks = async (): Promise<any> => {
-  const res = await client.get('');
-  return res.data;
+  return await client.get('');
 };
 
 export const deleteTask = async (id: number): Promise<null> => {
-  const res = await client.delete(`/${id}`);
-  return res.data;
+  return await client.delete(`/${id}`);
 };
 
 export const postTasks = async (todo: string): Promise<ITask> => {
-  const response = await client.post('', {
+  const res = await client.post('', {
     name: todo.trim(),
     completed: false,
   });
-  return response.data;
+  return res.data;
 };
 export const putTask = async (task: ITask): Promise<ITask> => {
-  const res = await client.put(`/${task.id}`, {
+  return await client.put(`/${task.id}`, {
     name: task.name,
     completed: !task.completed,
   });
-  return res.data;
 };
