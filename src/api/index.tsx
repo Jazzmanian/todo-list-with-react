551a0d1009948df9cd02b67bbf781e48a9da5d2c
JasmineZ <xiaoming.zhou@thoughtworks.com>
2022-12-14 22:27:06 +0800

    fix: redefine the API return type

--- src/api/index.tsx
+++ src/api/index.tsx
@@ -1,27 +1,27 @@
 import { ITask } from '../types';
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
 
 const client = axios.create({
   baseURL: process.env.REACT_APP_BASE_URL,
 });
 
-export const getTasks = async (): Promise<any> => {
-  return await client.get('');
+export const getTasks = async (): Promise<AxiosResponse<ITask[]>> => {
+  return await client.get<ITask[]>('');
 };
 
-export const deleteTask = async (id: number): Promise<null> => {
+export const deleteTask = async (id: number): Promise<AxiosResponse<void>> => {
   return await client.delete(`/${id}`);
 };
 
 export const postTasks = async (todo: string): Promise<ITask> => {
-  const res = await client.post('', {
+  const res = await client.post<ITask>('', {
     name: todo.trim(),
     completed: false,
   });
   return res.data;
 };
-export const putTask = async (task: ITask): Promise<ITask> => {
-  return await client.put(`/${task.id}`, {
+export const putTask = async (task: ITask): Promise<AxiosResponse<ITask>> => {
+  return await client.put<ITask>(`/${task.id}`, {
     name: task.name,
     completed: !task.completed,
   });
