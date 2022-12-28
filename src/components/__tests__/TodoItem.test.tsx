5b9a1a4be560579a5d50b996ac391c0d157b99c7
JasmineZ <xiaoming.zhou@thoughtworks.com>
2022-12-28 10:59:25 +0800

    test: complete TodoItem test

--- src/components/__tests__/TodoItem.test.tsx
+++ src/components/__tests__/TodoItem.test.tsx
@@ -1,28 +1,14 @@
 import { screen, fireEvent, render, waitFor } from '@testing-library/react';
-import { QueryClient, QueryClientProvider } from 'react-query';
-import { useTodos } from '../../hooks/useTodos';
 import TodoItem from '../TodoItem';
 
 const mockDeleteMutate = jest.fn();
 const mockCheckMutate = jest.fn();
 jest.mock('../../hooks/useTodos', () => ({
   useTodos: () => ({
-    deleteMutation: () => ({ mutate: mockDeleteMutate }),
-    checkMutation: () => ({ mutate: mockCheckMutate }),
+    deleteMutation: { mutate: mockDeleteMutate },
+    checkMutation: { mutate: mockCheckMutate },
   }),
 }));
-// jest.mock('react-query');
-// const mockedMutate = jest.fn();
-
-// jest.mock('../../hooks/useTodos', () => ({
-//   useTodos: () => ({
-//     deleteMutation: () => ({ mutate: jest.fn() }),
-//   }),
-// }));
-// const mockedUseCustomMutation = useTodos as jest.Mock;
-
-// expect(mockedMutate).toHaveBeenCalledTimes(1);
-
 describe('TodoItem test', () => {
   const mockTask = {
     id: 1,
@@ -33,11 +19,7 @@ describe('TodoItem test', () => {
   it('should call delete method with relevant task id', async () => {
     render(<TodoItem key={mockTask.id} task={mockTask} />);
     await fireEvent.click(screen.getByLabelText('delete-btn'));
-    // expect(screen.queryByDisplayValue('task name')).not.toBeInTheDocument();
     expect(mockDeleteMutate).toHaveBeenCalled();
-    // await waitFor(() => {
-    //   expect(mockedUseCustomMutation().mutate.mock.calls.length).toBe(1);
-    // });
   });
 
   it('should call check method with relevant task id', async () => {
