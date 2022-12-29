aeee334afb71742f2410c3522fca72f49da064e3
JasmineZ <xiaoming.zhou@thoughtworks.com>
2022-12-29 13:10:58 +0800

    fix: mock return data in TodoList test

--- src/components/__tests__/TodoList.test.tsx
+++ src/components/__tests__/TodoList.test.tsx
@@ -1,16 +1,21 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import TodoList from '../TodoList';
 
+jest.mock('../../hooks/useTodos', () => ({
+  useTodos: () => ({
+    data: [{ id: 1, name: 'task01', completed: true }],
+  }),
+}));
 describe('TodoList test', () => {
   it('should render task list when fetch the data', async () => {
     expect.assertions(1);
     const testQueryClient = new QueryClient();
-    const { ...result } = render(
+    render(
       <QueryClientProvider client={testQueryClient}>
         <TodoList />
       </QueryClientProvider>
     );
-    expect(await result.findByText(/task 01/i)).toBeInTheDocument();
+    expect(screen.getByText('task01')).toBeInTheDocument();
   });
 });
